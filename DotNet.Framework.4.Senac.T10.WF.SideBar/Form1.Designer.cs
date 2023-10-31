eb8310a9f31a5f2c5db74130ddd5e84aaba00481
Rafael Vieira Suarez <rafaelv_s@hotmail.com>
2023-10-30 21:48:41 -0300

    projeto

---
+++ DotNet.Framework.4.Senac.T10.WF.SideBar/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace DotNet.Framework._4.Senac.T10.WF.SideBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dashboard = new System.Windows.Forms.PictureBox();
+            this.sidebar = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.submenu = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.btnMenu = new System.Windows.Forms.Button();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timerSidebar = new System.Windows.Forms.Timer(this.components);
+            this.timerSubmenu = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dashboard)).BeginInit();
+            this.sidebar.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.submenu.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.dashboard);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1054, 85);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(106, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(299, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "WINDOWS FORMS | SideBar";
+            // 
+            // dashboard
+            // 
+            this.dashboard.Image = ((System.Drawing.Image)(resources.GetObject("dashboard.Image")));
+            this.dashboard.Location = new System.Drawing.Point(30, 12);
+            this.dashboard.Name = "dashboard";
+            this.dashboard.Size = new System.Drawing.Size(58, 59);
+            this.dashboard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dashboard.TabIndex = 0;
+            this.dashboard.TabStop = false;
+            this.dashboard.Click += new System.EventHandler(this.dashboard_Click);
+            // 
+            // sidebar
+            // 
+            this.sidebar.BackColor = System.Drawing.Color.Black;
+            this.sidebar.Controls.Add(this.panel2);
+            this.sidebar.Controls.Add(this.panel3);
+            this.sidebar.Controls.Add(this.panel4);
+            this.sidebar.Controls.Add(this.submenu);
+            this.sidebar.Controls.Add(this.panel5);
+            this.sidebar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sidebar.Location = new System.Drawing.Point(0, 85);
+            this.sidebar.Name = "sidebar";
+            this.sidebar.Size = new System.Drawing.Size(301, 705);
+            this.sidebar.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnDashboard);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(272, 90);
+            this.panel2.TabIndex = 2;
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.BackColor = System.Drawing.Color.Black;
+            this.btnDashboard.FlatAppearance.BorderSize = 0;
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDashboard.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDashboard.ForeColor = System.Drawing.Color.White;
+            this.btnDashboard.Image = ((System.Drawing.Image)(resources.GetObject("btnDashboard.Image")));
+            this.btnDashboard.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDashboard.Location = new System.Drawing.Point(15, 17);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(240, 60);
+            this.btnDashboard.TabIndex = 0;
+            this.btnDashboard.Text = "     Dashboard";
+            this.btnDashboard.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Location = new System.Drawing.Point(3, 99);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(272, 90);
+            this.panel3.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(15, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(240, 60);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "     Configurações";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button3);
+            this.panel4.Location = new System.Drawing.Point(3, 195);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(272, 90);
+            this.panel4.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(15, 17);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(240, 60);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "     Sobre";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // submenu
+            // 
+            this.submenu.Controls.Add(this.panel6);
+            this.submenu.Controls.Add(this.panel7);
+            this.submenu.Controls.Add(this.panel8);
+            this.submenu.Location = new System.Drawing.Point(3, 291);
+            this.submenu.Name = "submenu";
+            this.submenu.Size = new System.Drawing.Size(288, 93);
+            this.submenu.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.btnMenu);
+            this.panel6.Location = new System.Drawing.Point(3, 3);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(272, 81);
+            this.panel6.TabIndex = 3;
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.BackColor = System.Drawing.Color.Black;
+            this.btnMenu.FlatAppearance.BorderSize = 0;
+            this.btnMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMenu.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMenu.ForeColor = System.Drawing.Color.White;
+            this.btnMenu.Image = ((System.Drawing.Image)(resources.GetObject("btnMenu.Image")));
+            this.btnMenu.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMenu.Location = new System.Drawing.Point(15, 12);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(240, 60);
+            this.btnMenu.TabIndex = 0;
+            this.btnMenu.Text = "     Menu";
+            this.btnMenu.UseVisualStyleBackColor = false;
+            this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click);
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.button6);
+            this.panel7.Location = new System.Drawing.Point(3, 90);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(272, 77);
+            this.panel7.TabIndex = 4;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Black;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.Location = new System.Drawing.Point(15, 9);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(240, 60);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "     Submenu 1";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.button7);
+            this.panel8.Location = new System.Drawing.Point(3, 173);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(272, 78);
+            this.panel8.TabIndex = 4;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Black;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.Color.White;
+            this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
+            this.button7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button7.Location = new System.Drawing.Point(15, 7);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(240, 60);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "     Submenu 2";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.button4);
+            this.panel5.Location = new System.Drawing.Point(3, 390);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(272, 90);
+            this.panel5.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.Location = new System.Drawing.Point(15, 17);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(240, 60);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "     Sair";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // timerSidebar
+            // 
+            this.timerSidebar.Interval = 5;
+            this.timerSidebar.Tick += new System.EventHandler(this.timerSidebar_Tick);
+            // 
+            // timerSubmenu
+            // 
+            this.timerSubmenu.Interval = 5;
+            this.timerSubmenu.Tick += new System.EventHandler(this.timerSubmenu_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1054, 790);
+            this.Controls.Add(this.sidebar);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.IsMdiContainer = true;
+            this.Name = "Form1";
+            this.Text = "SIDEBAR";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dashboard)).EndInit();
+            this.sidebar.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.submenu.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel8.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox dashboard;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel sidebar;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnDashboard;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button btnMenu;
+        private System.Windows.Forms.FlowLayoutPanel submenu;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Timer timerSidebar;
+        private System.Windows.Forms.Timer timerSubmenu;
+    }
+}
+
---
+++ DotNet.Framework.4.Senac.T10.WF.SideBar/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DotNet.Framework._4.Senac.T10.WF.SideBar
+{
+    public partial class Form1 : Form
+    {
+        #region 1.Contrutores
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #endregion 1.Contrutores
+
+        #region 2.Variaveis
+        // criei uma variavel booleana que tem o valor true
+        bool sidebarExpandida = true;
+        bool submenuExpandido = false;
+
+        #endregion 2.Variaveis
+
+        #region Eventos
+        private void dashboard_Click(object sender, EventArgs e)
+        {
+            timerSidebar.Start();
+        }
+
+        private void btnMenu_Click(object sender, EventArgs e)
+        {
+            timerSubmenu.Start();
+        }
+
+        private void timerSidebar_Tick(object sender, EventArgs e)
+        {
+            if (sidebarExpandida)
+            {
+                sidebar.Width -= 10;
+                if(sidebar.Width <= 87)
+                {
+                    sidebarExpandida = false;
+                    timerSidebar.Stop();
+                    return;
+                }    
+            }
+            else
+            {
+                sidebar.Width += 10;
+                if(sidebar.Width >= 299)
+                {
+                    sidebarExpandida = true;
+                    timerSidebar.Stop();
+                    return;
+                }     
+            }
+        }
+        #endregion Eventos
+
+        #region Metodos
+
+        string RetornaMensagem()
+        {
+            return "";
+        }
+        #endregion Metodos
+
+        private void timerSubmenu_Tick(object sender, EventArgs e)
+        {   // se o submenu esta expandido
+            if(submenuExpandido)
+            {   // diminui a altura em 10 pixel's
+                submenu.Height -= 10;
+                // se a altura atual ja está no minimo
+                if(submenu.Height <= 86)
+                {   // paro o timer
+                    submenuExpandido = false;
+                    timerSubmenu.Stop();
+                }
+            }
+            else
+            {   // aumento a altura em 10 pixel's
+                submenu.Height += 10;
+
+                if(submenu.Height >= 256)
+                {
+                    submenuExpandido = true;
+                    timerSubmenu.Stop();
+                }
+            }
+        }
+    }
+}
